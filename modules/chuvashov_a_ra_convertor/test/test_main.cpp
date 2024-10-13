c58d663bd1e68dcc44929cd2140086acc85dc8aa
PavelLSmirnov <pavel1201s@yandex.ru>
2024-10-13 16:19:58 +0300

    fix delete errored lab

--- modules/chuvashov_a_ra_convertor/test/test_main.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2024 Chuvashov Andrey
-
-#include "../../../3rdparty/gtest/gtest.h"
-
-int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
